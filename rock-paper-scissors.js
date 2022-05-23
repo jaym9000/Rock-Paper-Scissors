390c0ee094e3f0301b6e7f8017b7131f60f4819d
Jay-M <jean_marcc@hotmail.com>
2022-05-22 21:28:33 -0300

    HTML and JS files made, computerPlay function randomly prints Rock Paper Scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+function computerPlay() {
+  let hand = ["Rock", "Paper", "Scissors"];
+  return hand[Math.floor(Math.random() * hand.length)];
+}
+
+// console.log(computerPlay());
