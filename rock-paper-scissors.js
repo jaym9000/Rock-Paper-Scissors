306b31cb3e902b9856f7814877bf514da0ecf86d
Jay-M <jean_marcc@hotmail.com>
2022-05-23 21:46:15 -0300

    singleRound function done

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
 console.log("Hello World");
 
 function computerPlay() {
+  //computer plays random between rock paper and scissors
   let hand = ["Rock", "Paper", "Scissors"];
   return hand[Math.floor(Math.random() * hand.length)];
 }
@@ -8,10 +9,35 @@ function computerPlay() {
 // console.log(computerPlay());
 
 function singleRound(playerSelection, computerSelection) {
+  //playerSelection case-insensitive
   let hand = playerSelection.charAt(0).toUpperCase();
   hand += playerSelection.substring(1).toLowerCase();
 
-  return hand;
+  //   return hand;
+  console.log(
+    "You choose: " + hand + " | The computer choose: " + computerSelection
+  );
+
+  if (hand === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (hand === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+    // 2 statements above have player select Rock
+  } else if (hand === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (hand === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beat paper";
+    // 2 statements above have player select Paper
+  } else if (hand === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (hand === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beat paper";
+    // 2 statements above have player select Scissors
+  } else {
+    return "You've got a tie";
+  }
 }
 
-console.log(singleRound("rOCK"));
+console.log(singleRound("rOCK", computerPlay()));
+
+// hand === "Paper" && computerSelection === "Rock"
