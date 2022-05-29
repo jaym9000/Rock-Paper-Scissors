7bb5a640fb49bfc464316ba0571d7d16b6e80afd
Jay-M <jean_marcc@hotmail.com>
2022-05-29 15:36:13 -0300

    DOM methods added to project to make text results display on screen

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,18 @@
+console.log("Hello");
+
 console.log("Welcome to my Rock - Paper - Scissors game!");
 
 //initializations and declarations
 let you = 0;
 let computerCount = 0;
 let cpu; //this is the computer's choice
+
+//display beginning score of 0 - 0
+const results = document.querySelector('#results');
+const resultText = document.createElement('div');
+resultText.classList.add('resultText');
+resultText.textContent = "Score: You = " + you + " | CPU = " + computerCount;
+results.appendChild(resultText);
 
 //computer plays random between rock paper and scissors
 function computerPlay() {
@@ -23,20 +32,31 @@ const rockBtn = document.querySelector(".Rock");
 const paperBtn = document.querySelector(".Paper");
 const scissorsBtn = document.querySelector(".Scissors");
 
+//DOM methods
+const score = document.createElement('div');
+resultText.classList.add('resultText');
+  score.classList.add('score');
+
 //Rock button
 rockBtn.addEventListener("click", function(){  
   cpu = computerPlay();
   playRound('rock', cpu);
-
+  
+  //display results to screen
   if (cpu === "Paper") {
     computerCount += 1;
-    console.log("You Lose this round! Paper beats Rock.");
+    resultText.textContent = "You Lose this round! Paper beats Rock.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   } else if (cpu === "Scissors") {
     you += 1;
-    console.log("You Win this round! Rock beats Scissors.");
+    resultText.textContent = "You Win this round! Rock beats Scissors.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   }else {
-    console.log("You tied this round.");
+    resultText.textContent = "You tied this round.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   }
+  results.appendChild(resultText);
+  results.appendChild(score);
 });
 
 //paper button
@@ -44,14 +64,18 @@ paperBtn.addEventListener("click", function(){
   cpu = computerPlay();
   playRound('paper', cpu);
 
+  //display results to screen
   if (cpu === "Rock") {
     you += 1;
-    console.log("You Win this round! Paper beats Rock.");
+    resultText.textContent = "You Win this round! Paper beats Rock.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   } else if (cpu === "Scissors") {
     computerCount += 1;
-    console.log("You Lose this round! Scissors beat Paper.");
+    resultText.textContent = "You Lose this round! Scissors beat Paper.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   }else {
-    console.log("You tied this round.");
+    resultText.textContent = "You tied this round.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   }
 });
 
@@ -60,36 +84,17 @@ scissorsBtn.addEventListener("click", function(){
   cpu = computerPlay();
   playRound('Scissors', cpu);
 
+  //display results to screen
   if (cpu === "Rock") {
     computerCount += 1;
-    console.log("You Lose this round! Rock beats Scissors.");
+    resultText.textContent = "You Lose this round! Rock beats Scissors.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   } else if (cpu === "Paper") {
     you += 1;
-    console.log("You Win this round! Scissors beat Paper.");
+    resultText.textContent = "You Win this round! Scissors beat Paper.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   } else {
-    console.log("You tied this round.");
+    resultText.textContent = "You tied this round.";
+    score.textContent = "Score: You = " + you + " | CPU = " + computerCount;
   }
 });
-
-//Run the game
-// function game() {
-//     cpu = computerPlay();
-    
-//     // console.log("The Computer choose: " + cpu);
-//     // console.log(playRound(playerInput, cpu));
-//     // console.log("Your score: " + you + " | Computer score: " + computerCount);
-    
-//   // if (you > computerCount) {
-//   //   console.log("You have won the game!!");
-//   // } else if (you === computerCount) {
-//   //   console.log("You have tied the computer.");
-//   // } else {
-//   //   console.log("You lost... get back UP and try again!");
-//   // }
-// }
-
-// //line below runs the game
-
-// if(rockBtn.clicked === true || paperBtn.clicked === true || scissorsBtn.clicked === true){
-//   game();
-// }
